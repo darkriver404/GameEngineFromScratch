5faf8cbf84a2736d60019559c58d319d4c4ab500
darkriver404 <darkriver404@outlook.com>
2018-08-07 18:14:06 +0800

    准备环境

---
+++ mingw-w64.bat
@@ -0,0 +1,6 @@
+echo off
+set PATH=D:\mingw-w64\x86_64-8.1.0-win32-seh-rt_v6-rev0\mingw64\bin;%PATH%
+rem echo %PATH%
+rem cd "D:\mingw-w64\x86_64-8.1.0-win32-seh-rt_v6-rev0\mingw64\bin"
+cd "E:\GameEngineFromScratch"
+"C:\Windows\system32\cmd.exe"
