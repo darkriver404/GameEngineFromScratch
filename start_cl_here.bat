9d22b1382029aafe27755288501f6c67b0962d05
darkriver404 <darkriver404@outlook.com>
2018-08-10 15:27:59 +0800

    添加cl快速启动

---
+++ start_cl_here.bat
@@ -0,0 +1,6 @@
+echo off
+set PATH=D:\Program Files\Microsoft VS Code;%PATH%
+%comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"" x86
+
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+REM "C:\Windows\system32\cmd.exe"
